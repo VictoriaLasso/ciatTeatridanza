b0ed62181c4b26de632a4f2d8f450099b8a065ef
Victoria Lasso <maria.lasso@imexhs.com>
2020-07-24 15:24:30 -0500

    Clear cache

--- cache/gantry5/rt_manticore/html/646d831703dc8f38d0c12d8af62b9f99.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
-    '_version' => 1,
-    'id' => '5f1a015e87d0d1.16690140',
-    'content' => '<div id="mobile-menu-3516-particle" class="g-content g-particle">            <div id="g-mobilemenu-container" data-g-menu-breakpoint="51rem"></div>
-            </div>'
-];
--- cache/gantry5/rt_manticore/html/b9ba10727ba3108e2ffd970bde1a81bf.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    '_type' => 'Gantry\\Component\\Content\\Block\\HtmlBlock',
-    '_version' => 1,
-    'id' => '5f1a02b9ac8760.79824798',
-    'content' => '<div class="spacer"></div>'
-];
